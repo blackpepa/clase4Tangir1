914ca53b0c81c98c42b858baf4f105cc89a541c6
cecilia <ceciliatangir@gmail.com>
2022-05-16 20:31:47 -0300

    fix final?

--- js/sim.js
+++ js/sim.js
@@ -2,49 +2,49 @@ function horoscopo(signoUser){
     let horoscopo = " ";
     switch(signoUser){
         case "aries":
-            horoscopo ="comerás buen jamón crudo";
+            horoscopo ="comerás buen jamón crudo. ";
             break;
         case "tauro":
-            horoscopo = "tu inodoro quedará reluciente";
+            horoscopo = "tu inodoro quedará reluciente. ";
             break;
         case "geminis":
-            horoscopo ="llamarás a tu mamá";
+            horoscopo ="llamarás a tu mamá. ";
             break;
         case "géminis":
-            horoscopo ="llamarás a tu mamá";
+            horoscopo ="llamarás a tu mamá. ";
             break;
         case "cancer":
-            horoscopo = "club día te dará un descuento en papas fritas";
+            horoscopo = "club día te dará un descuento en papas fritas. ";
             break;
         case "cáncer":
-            horoscopo = "club día te dará un descuento en papas fritas";
+            horoscopo = "club día te dará un descuento en papas fritas. ";
             break;
         case "leo":
-            horoscopo = "tomarás sol al mediodía";
+            horoscopo = "tomarás sol al mediodía. ";
             break;
         case "virgo":
-            horoscopo = "te quedarás sin papel higiénico";
+            horoscopo = "te quedarás sin papel higiénico. ";
             break;
         case "libra":
-            horoscopo = "ese jean te volverá a entrar";
+            horoscopo = "ese jean te volverá a entrar. ";
             break;
         case "escorpio":
-            horoscopo ="cantarás todo el día una canción de Arjona";
+            horoscopo ="cantarás todo el día una canción de Arjona. ";
             break;
         case "sagitario":
-            horoscopo= "tendrás que correr el bondi";
+            horoscopo= "tendrás que correr el bondi.";
             break;
         case "capricornio":
-            horoscopo="pisarás caca de perro";
+            horoscopo="pisarás caca de perro. ";
             break;
         case "acuario":
-            horoscopo="te caerá mal ese sushi";
+            horoscopo="te caerá mal ese sushi. ";
             break;
         case "piscis":
-            horoscopo="seguirás igual de hippie";
+            horoscopo="seguirás igual de hippie.";
             break;
          default:
-            horoscopo ="pruebes con un signo del zodiaco";
+            horoscopo ="pruebes con un signo del zodiaco. ";
     }
     return horoscopo
     }
@@ -53,10 +53,12 @@ let signoUser = signoIngresado.toLocaleLowerCase();
 let unHoroscopo = "Madame LeFortune dice que " + horoscopo(signoUser);
 alert (unHoroscopo);
 
-
 while(signoIngresado !== horoscopo(signoUser)){
     signoIngresado = prompt("Ingresá un signo astrológico válido");
     signoUser = signoIngresado.toLocaleLowerCase();
     unHoroscopo = "Madame LeFortune dice que " + horoscopo(signoUser);
-    alert (unHoroscopo);
+    let nuevaentrada = prompt (unHoroscopo + " Si querés hacer otra consulta ingresá el signo, sino cancelá.");
+     if(nuevaentrada != " "){
+         break;
+     }
 }
